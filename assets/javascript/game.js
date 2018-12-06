ebfd98d650cec6b973b79702b16d4bfbc405b189
Perla Ordonez <guate.po@gmail.com>
2018-12-06 16:38:56 -0600

    updated counters for wins/ losses and added array for user guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,29 @@ var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 // variable holds the number of wins, losses, and remaining guesses
 var wins = 0,
     losses = 0,
-    guessesLeft = 10;
-// holds the users guesses
-var guessArray = [];
+    guessesLeft = 9;
+// empty array for user guesses
+var guessedLetters =[];
 // refrences to the places in HTML where we are going to display things
 var userChoiceText = document.getElementById("userchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-left");
-
+// compares users guess and computers guess and adds points to wins or losses
 document.onkeyup = function(event){
-    var userGuess = event.key;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
-    if (userGuess === computerGuess){
-        wins++;
-    }else{
-        losses++;
-        guessesLeft--;
-    }
-
+        if (userGuess === computerGuess){
+            wins++;
+        }else{
+            losses++;
+            guessesLeft--;
+            guessedLetters.push(userGuess);
+        }
+    
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     guessesText.textContent = "Guesses left: " + guessesLeft;
-    userChoice.textContent = "Your guesses so far: " + userGuess;
+    userChoiceText.textContent= "Your guesses so far: " + guessedLetters
+
 }
